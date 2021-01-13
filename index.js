01d09dc4ad5d53f2269edbad03f8b5fdf0292ab7
dornescum <dornescu@gmail.com>
2021-01-13 15:04:45 +0200

    refacut butoane index / refacut foote/ refacut modal

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ const getElement = (selection) => {
 const toggleNav = getElement('.toggle-nav');
 const sidebarOverlay = getElement('.sidebar-overlay');
 const closeBtn = getElement('.sidebar-close');
+
 toggleNav.addEventListener('click', () => {
     sidebarOverlay.classList.add('show')
 })
@@ -30,3 +31,41 @@ window.addEventListener("DOMContentLoaded", function () {
     setTimeout(hideLoading, 100);
     // showProjects()
 })
+
+
+
+// const divAboutMe = document.createElement("div")
+// divAboutMe.classList.add("text-center")
+//
+// const btnAboutMe = document.createElement("button")
+// btnAboutMe.classList.add("hero-btn-display")
+// btnAboutMe.innerHTML ="push"
+//
+// btnAboutMe.append(divAboutMe)
+//  btnAboutMe.style.backgroundColor ="red"
+// console.log(btnAboutMe)
+// sectionHero.appendChild(divAboutMe)
+//
+// btnAboutMe.addEventListener("click", ()=>{
+//     container.classList.toggle("invisible")
+//     // progress.classList.toggle("visible")
+// })
+
+
+// const btnUp = document.createElement("button")
+// btnUp.classList.add("btn-up")
+// btnUp.innerText= " Up"
+//
+// btnUp.addEventListener("click", scrollToTop)
+// main.appendChild(btnUp)
+//
+// let rootElement = document.documentElement
+// function scrollToTop() {
+//     // Scroll to top logic
+//     rootElement.scrollTo({
+//         top: 0,
+//         behavior: "smooth"
+//     })
+// }
+
+
