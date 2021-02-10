324acdcccd37a875b3bdee931c043adacf322b91
dornescum <dornescu@gmail.com>
2021-02-10 18:02:20 +0200

    poze webp dimensiune

--- js/module/photosWork.js
+++ js/module/photosWork.js
@@ -3,63 +3,63 @@
 export const photos = [
     {
         id: "p1",
-        foto: "@../../img/webp/1.webp",
+        foto: "@../../img/webp2/1.webp",
         title: "",
         location: "",
         dataTooltip: "2005"
     },
     {
         id: "p2",
-        foto: "@../../img/webp/2.webp",
+        foto: "@../../img/webp2/2.webp",
         title: "",
         location: "",
         data: "2005"
     },
     {
         id: "p3",
-        foto: "@../../img/webp/4.webp",
+        foto: "@../../img/webp2/4.webp",
         title: "",
         location: "",
         data: "2005"
     },
     {
         id: "p4",
-        foto: "@../../img/webp/6-min.webp",
+        foto: "@../../img/webp2/6-min.webp",
         title: "",
         location: "",
         data: "2005"
     },
     {
         id: "p5",
-        foto: "@../../img/webp/10-min.webp",
+        foto: "@../../img/webp2/10-min.webp",
         title: "",
         location: "",
         data: "2005"
     },
     {
         id: "p6",
-        foto: "@../../img/webp/7.webp",
+        foto: "@../../img/webp2/7.webp",
         title: "",
         location: "",
         data: "2005"
     },
     {
         id: "p7",
-        foto: "@../../img/webp/09.webp",
+        foto: "@../../img/webp2/09.webp",
         title: "",
         location: "",
         data: "2005"
     },
     {
         id: "p8",
-        foto: "@../../img/webp/barca.webp",
+        foto: "@../../img/webp2/barca.webp",
         title: "",
         location: "",
         data: "2005"
     },
     {
         id: "p9",
-        foto: "@../../img/webp/tortelini-min.webp",
+        foto: "@../../img/webp2/tortelini-min.webp",
         title: "",
         location: "",
         data: "2005"
--- js/module/testimonials.js
+++ js/module/testimonials.js
@@ -4,7 +4,7 @@ export let testimonials = [
         title: 'Worst chef ever',
         nume: "Akemi",
         data: '01.06.2425',
-        text:"THIS IS JUST A JOKE .I used an array method: map.(), destructure an object and return the content of an" +
+        text:"THIS IS JUST A JOKE. I used an array method: map.(), destructure an object and return the content of an" +
             " array of objects with funny reviews."
     },
     {
@@ -12,13 +12,15 @@ export let testimonials = [
         nume: "Emiyo",
         title: 'Best food ever',
         data: '01.1.24254',
-        text: ' Lorem Ipsumer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.'
+        text:"THIS IS JUST A JOKE. I used an array method: map.(), destructure an object and return the content of an" +
+            " array of objects with funny reviews."
     },
     {
         id: 3,
         nume: "Risako",
         title: 'I had worst &#128540',
         data: '11.01.2425323',
-        text: 'Lorem Ipsum   It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.'
+        text:"THIS IS JUST A JOKE. I used an array method: map.(), destructure an object and return the content of an" +
+            " array of objects with funny reviews."
     }
 ]
