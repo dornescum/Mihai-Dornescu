fb9ba09a9e93911f819a94088fc51e0447baa511
dornescum <dornescu@gmail.com>
2021-02-10 13:24:53 +0200

    poze gasite

--- js/module/photosWork.js
+++ js/module/photosWork.js
@@ -3,14 +3,14 @@
 export const photos = [
     {
         id: "p1",
-        foto: "../../img/webp/1.webp",
+        foto: "@../../img/webp/1.webp",
         title: "",
         location: "",
         dataTooltip: "2005"
     },
     {
         id: "p2",
-        foto: "../../img/webp/2.webp",
+        foto: "@../../img/webp/2.webp",
         title: "",
         location: "",
         data: "2005"
@@ -24,14 +24,14 @@ export const photos = [
     },
     {
         id: "p4",
-        foto: "../../img/webp/6-min.webp",
+        foto: "@../../img/webp/6-min.webp",
         title: "",
         location: "",
         data: "2005"
     },
     {
         id: "p5",
-        foto: "../../img/webp/10-min.webp",
+        foto: "@../../img/webp/10-min.webp",
         title: "",
         location: "",
         data: "2005"
