1c9c58c667acc38e2b8920c980d3fdb71c849b28
Mihai Dornescu <dornescu@gmail.com>
2022-01-31 10:36:11 +0200

    conditioanl link

--- src/js/index.js
+++ src/js/index.js
@@ -31,7 +31,7 @@ const projects = [{
 	description: "An admin panel in which I tested Cypress together with React js.",
 	tools: "React js, React-router, Styled-Components, Cypress, Context Api",
 	linkRepo: "https://github.com/dornescum/adminPannel",
-	linkPage: "https://dornescum.github.io/Mihai-Dornescu/",
+	linkPage: "",
 	img: "@../../src/img/projects/github.webp"
 	},
 	{
@@ -47,6 +47,39 @@ const projects = [{
 const projectsGrid = getElement('#grid-projects');
 
 projectsGrid.innerHTML = projects.map((item) => {
+	if (item.linkPage === ''){
+		return `
+		<div class="flex flex-col mx-0 md:mx-6 lg:mx-8" data-testid=${item?.id}>
+                <div class="">
+                    <div>
+                        <img alt="picture"
+                             src=${item?.img} class="h-60 md:h-48 lg:h-96 w-full object-cover rounded-tr-md rounded-tl-md
+                shadow-sm hover:shadow-lg cursor-pointer transition duration:300">
+                    </div>
+                    <div class=" border p-1 rounded-br-md rounded-bl-md h-80 md:h-90 lg:h-72">
+                        <article class="text-stone-500" data-testid=${item?.title.slice(0, 3)}>
+                            <h2 class="text-center font-bold p-1">${item?.title}</h2>
+                            <ul class="text-justify">
+                                <li class="leading-relaxed py-4 px-2">${item?.description}</li>
+                                <li class="py-2 px-2 hover:text-red-300 cursor-pointer">${item?.tools}</li>
+                                <li class="py-2 px-2">
+                                    <div>
+                                        <p  class="inline-flex">
+                                            Repo:
+                                        </p>
+                                        <a href=${item?.linkRepo} target="_blank" rel="noopener" class="inline-flex">Github</a>
+                                    </div>
+                                </li>
+                                <li class="pt-2 pb-4 px-2 ">
+                                    <a href='javascript:;'  rel="noopener" class="text-stone-700 animate-pulse">No website just a repo</a>
+                                </li>
+                            </ul>
+                        </article>
+                    </div>
+                </div>
+            </div>
+		`
+	}
 	return `         
 	        <div class="flex flex-col mx-0 md:mx-6 lg:mx-8" data-testid=${item?.id}>
                 <div class="">
@@ -66,11 +99,11 @@ projectsGrid.innerHTML = projects.map((item) => {
                                         <p  class="inline-flex">
                                             Repo:
                                         </p>
-                                        <a href=${item?.linkRepo} target="_blank"  class="inline-flex">Github</a>
+                                        <a href=${item?.linkRepo} target="_blank" rel="noopener" class="inline-flex">Github</a>
                                     </div>
                                 </li>
                                 <li class="pt-2 pb-4 px-2 ">
-                                    <a href=${item?.linkPage} target="_blank" class="text-stone-700 animate-pulse">Visit website</a>
+                                    <a href=${item?.linkPage} target="_blank" rel="noopener" class="text-stone-700 animate-pulse">Visit website</a>
                                 </li>
                             </ul>
                         </article>
