ef9d7c164125e85886d1bc6087753c47912961b2
Mihai Dornescu <dornescu@gmail.com>
2022-01-31 17:23:21 +0200

    card 6&7

--- src/js/Projects.js
+++ src/js/Projects.js
@@ -1,4 +1,4 @@
-export const  projects = [{
+export const projects = [{
 	id: 1,
 	title: "Mock Up Delivery Website",
 	description: "An exercise in which I used Next js and Tailwind Css to create a website for an imaginary company that produces and processes food. Using ISR & SSR, hybrid.",
@@ -14,33 +14,47 @@ export const  projects = [{
 	linkRepo: "https://github.com/dornescum/tailwind-nextJs-setup",
 	linkPage: "https://car-next.vercel.app/",
 	img: "@../../src/img/projects/opelDan.webp"
+}, {
+	id: 3,
+	title: "Admin Panel",
+	description: "An admin panel in which I tested Cypress together with React js.",
+	tools: "React js, React-router, Styled-Components, Cypress, Context Api",
+	linkRepo: "https://github.com/dornescum/adminPannel",
+	linkPage: "",
+	img: "@../../src/img/projects/github.webp"
+}, {
+	id: 4,
+	title: "Cypress testing FirmeCadastru.ro",
+	description: "Cypress testing FirmeCadastru.ro",
+	tools: "React js, React-router, Styled-Components, Cypress, Context Api",
+	linkRepo: "https://github.com/dornescum/FC",
+	linkPage: "https://firmecadastru.ro/",
+	img: "@../../src/img/projects/cypress.webp"
+}, {
+	id: 5,
+	title: "Todo App",
+	description: "A simple typescript app, from freeCodeCamp.org",
+	tools: "Typescript",
+	linkRepo: "https://github.com/dornescum/Todo-tsx",
+	linkPage: "https://todo-app-cc92d.web.app/",
+	img: "@../../src/img/projects/task_app.webp"
+}, {
+	id: 6,
+	title: "Card",
+	description: "Basic article card, challenge from FrontEndMentor.io",
+	tools: "Html, Scss",
+	linkRepo: "https://github.com/dornescum/FrontEndMentor-article",
+	linkPage: "https://front-end-mentor-article.vercel.app/",
+	img: "@../../src/img/projects/article_card.webp"
 },
 	{
-		id: 3,
-		title: "Admin Panel",
-		description: "An admin panel in which I tested Cypress together with React js.",
-		tools: "React js, React-router, Styled-Components, Cypress, Context Api",
-		linkRepo: "https://github.com/dornescum/adminPannel",
-		linkPage: "",
-		img: "@../../src/img/projects/github.webp"
-	},
-	{
-		id: 4,
-		title: "Cypress testing FirmeCadastru.ro",
-		description: "Cypress testing FirmeCadastru.ro",
-		tools: "React js, React-router, Styled-Components, Cypress, Context Api",
-		linkRepo: "https://github.com/dornescum/FC",
-		linkPage: "https://firmecadastru.ro/",
-		img: "@../../src/img/projects/cypress.webp"
-	},
-	{
-		id: 5,
-		title: "Todo App",
-		description: "A simple typescript app, from freeCodeCamp.org",
-		tools: "Typescript",
-		linkRepo: "https://github.com/dornescum/Todo-tsx",
-		linkPage: "https://todo-app-cc92d.web.app/",
-		img: "@../../src/img/projects/task_app.webp"
+		id: 7,
+		title: "Card",
+		description: "Equilibrium card, challenge from FrontEndMentor.io",
+		tools: "Html, Scss",
+		linkRepo: "https://github.com/dornescum/Frontend-mentor-NFT",
+		linkPage: "https://nft-beige.vercel.app/",
+		img: "@../../src/img/projects/challenge-card.webp"
 	},
 
 ];
