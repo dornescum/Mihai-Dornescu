c3814837f2e506aed512fffaf7f6fc01033bb7a3
Mihai Dornescu <dornescu@gmail.com>
2022-02-15 10:38:01 +0200

    add moviesdb projects

--- src/js/utils/Projects.js
+++ src/js/utils/Projects.js
@@ -8,17 +8,18 @@ export const projects = [{
 	img: "@../../src/img/projects/mockUpMenu.webp",
 	tagJs: "Next",
 	tagCss: "Tailwind"
-}, {
-	id: 2,
-	title: "Best Car Ever",
-	description: "An exercise in which I used Next js and Scss to create a website where I sell a car.",
-	tools: "Next js, Scss, Simple-react-lightbox, React Icons",
-	linkRepo: "https://github.com/dornescum/car_next",
-	linkPage: "https://car-next.vercel.app/",
-	img: "@../../src/img/projects/opelDan.webp",
-	tagJs: "Next",
-	tagCss: "Scss"
 },
+	{
+		id: 2-15,
+		title: "Movie search",
+		description: "Fetching movies from DB and search for the movies",
+		tools: "React js, Tailwind Css",
+		linkRepo: "https://github.com/dornescum/moviedDb",
+		linkPage: "https://movie-db-188e7.web.app/",
+		img: "@../../src/img/projects/movieDb.webp",
+		tagJs: "React",
+		tagCss: "Tailwind",
+	},
 	{
 		id: 12,
 		title: "Menu cart",
@@ -32,67 +33,79 @@ export const projects = [{
 	},
 
 	{
-	id: 3,
-	title: "Admin Panel",
-	description: "An admin panel in which I tested Cypress together with React js.",
-	tools: "React js, Styled-Components, Cypress, Context Api",
-	linkRepo: "https://github.com/dornescum/adminPannel",
-	linkPage: "",
-	img: "@../../src/img/projects/cypress.webp",
-	tagJs: "Cypress",
-	tagCss: "Styled-Components"
-}, {
-	id: 4,
-	title: "Cypress testing FirmeCadastru.ro",
-	description: "Cypress testing FirmeCadastru.ro",
-	tools: "Cypress",
-	linkRepo: "https://github.com/dornescum/FC",
-	linkPage: "https://firmecadastru.ro/",
-	img: "@../../src/img/projects/cypress.webp",
-	tagJs: "Cypress",
-	tagCss: "Css"
-}, {
-	id: 5,
-	title: "Todo App",
-	description: "A simple todo app with typescript , from freeCodeCamp.org.",
-	tools: "Typescript",
-	linkRepo: "https://github.com/dornescum/Todo-tsx",
-	linkPage: "https://todo-app-cc92d.web.app/",
-	img: "@../../src/img/projects/task_app.webp",
-	tagJs: "React",
-	tagCss: "Css",
-	tagTs: "Typescript"
-}, {
-	id: 6,
-	title: "Card",
-	description: "Basic article card, challenge from FrontEndMentor.io.",
-	tools: "Html, Scss",
-	linkRepo: "https://github.com/dornescum/FrontEndMentor-article",
-	linkPage: "https://front-end-mentor-article.vercel.app/",
-	img: "@../../src/img/projects/article_card.webp",
-	tagJs: "Html",
-	tagCss: "Scss"
-}, {
-	id: 7,
-	title: "Card",
-	description: "Equilibrium card, challenge from FrontEndMentor.io.",
-	tools: "Html, Scss",
-	linkRepo: "https://github.com/dornescum/Frontend-mentor-NFT",
-	linkPage: "https://nft-beige.vercel.app/",
-	img: "@../../src/img/projects/challenge-card.webp",
-	tagJs: "Html",
-	tagCss: "Scss"
-}, {
-	id: 8,
-	title: "Google Clone",
-	description: "Google clone exercise from jS Mastery, Context Api, useDebounce,Tailwind darkMode.",
-	tools: "React js, Tailwind Css",
-	linkRepo: "https://github.com/dornescum/google-clone-jsMastery",
-	linkPage: "",
-	img: "@../../src/img/projects/googl.webp",
-	tagJs: "React",
-	tagCss: "Tailwind"
-},
+		id: 3,
+		title: "Admin Panel",
+		description: "An admin panel in which I tested Cypress together with React js.",
+		tools: "React js, Styled-Components, Cypress, Context Api",
+		linkRepo: "https://github.com/dornescum/adminPannel",
+		linkPage: "",
+		img: "@../../src/img/projects/cypress.webp",
+		tagJs: "Cypress",
+		tagCss: "Styled-Components"
+	}, {
+		id: 4,
+		title: "Cypress testing FirmeCadastru.ro",
+		description: "Cypress testing FirmeCadastru.ro",
+		tools: "Cypress",
+		linkRepo: "https://github.com/dornescum/FC",
+		linkPage: "https://firmecadastru.ro/",
+		img: "@../../src/img/projects/cypress.webp",
+		tagJs: "Cypress",
+		tagCss: "Css"
+	},
+	{
+		id: 2,
+		title: "Best Car Ever",
+		description: "An exercise in which I used Next js and Scss to create a website where I sell a car.",
+		tools: "Next js, Scss, Simple-react-lightbox, React Icons",
+		linkRepo: "https://github.com/dornescum/car_next",
+		linkPage: "https://car-next.vercel.app/",
+		img: "@../../src/img/projects/opelDan.webp",
+		tagJs: "Next",
+		tagCss: "Scss"
+	},
+	{
+		id: 5,
+		title: "Todo App",
+		description: "A simple todo app with typescript , from freeCodeCamp.org.",
+		tools: "Typescript",
+		linkRepo: "https://github.com/dornescum/Todo-tsx",
+		linkPage: "https://todo-app-cc92d.web.app/",
+		img: "@../../src/img/projects/typescript.webp",
+		tagJs: "React",
+		tagCss: "Css",
+		tagTs: "Typescript"
+	}, {
+		id: 6,
+		title: "Card",
+		description: "Basic article card, challenge from FrontEndMentor.io.",
+		tools: "Html, Scss",
+		linkRepo: "https://github.com/dornescum/FrontEndMentor-article",
+		linkPage: "https://front-end-mentor-article.vercel.app/",
+		img: "@../../src/img/projects/article_card.webp",
+		tagJs: "Html",
+		tagCss: "Scss"
+	}, {
+		id: 7,
+		title: "Card",
+		description: "Equilibrium card, challenge from FrontEndMentor.io.",
+		tools: "Html, Scss",
+		linkRepo: "https://github.com/dornescum/Frontend-mentor-NFT",
+		linkPage: "https://nft-beige.vercel.app/",
+		img: "@../../src/img/projects/challenge-card.webp",
+		tagJs: "Html",
+		tagCss: "Scss"
+	}, {
+		id: 8,
+		title: "Google Clone",
+		description: "Google clone exercise from <b>JavaScriptSMastery</b>, Context Api, useDebounce,Tailwind darkMode.",
+		tools: "React js, Tailwind Css",
+		linkRepo: "https://github.com/dornescum/google-clone-jsMastery",
+		linkPage: "",
+		img: "@../../src/img/projects/googl.webp",
+		tagJs: "React",
+		tagCss: "Tailwind"
+	},
 	{
 		id: 10,
 		title: "Vuetify",
@@ -118,7 +131,7 @@ export const projects = [{
 	{
 		id: 11,
 		title: "Shopping Cart",
-		description: "Context Api & useReducer for state management from freeCodeCamp.org",
+		description: "Context Api & useReducer for state management from <b>RoadsideCoder</b>",
 		tools: "React js, React-Bootstrap",
 		linkRepo: "https://github.com/dornescum/shoppingCart",
 		linkPage: "",
@@ -126,5 +139,18 @@ export const projects = [{
 		tagJs: "React",
 		tagCss: "Bootstrap"
 	},
+	{
+		id: 13,
+		title: "Add List",
+		description: "React Typescript passing props, interface exercise from <b>Laith Harb</b>",
+		tools: "React js, React-Bootstrap",
+		linkRepo: "https://github.com/dornescum/react-props-typescript",
+		linkPage: "",
+		img: "@../../src/img/projects/typescript.webp",
+		tagJs: "React",
+		tagCss: "Tailwind",
+		tagTs: "Typescript"
+	},
+
 
 ];
