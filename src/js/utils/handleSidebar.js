44016516c94a713d6971e0aa2bd2c00800f5916d
dornescu mihai <dornescu@gmail.com>
2024-05-07 11:05:31 +0300

    Removed video link from sidebar and updated workspace settings
    Removed the video page link from the sidebar navigation in the handleSidebar.js file. Additionally, streamlined the order of recent templates in the workspace.xml and updated the local tasks counter. Several commit messages were also removed for simplicity.

--- src/js/utils/handleSidebar.js
+++ src/js/utils/handleSidebar.js
@@ -72,14 +72,6 @@ export const handleSidebar = (access, sidebar) => {
                         Contact
                     </a>
                 </li>
-                 <li class="block py-2 md:py-4 pl-4 md:pl-8  lg:pl-12">
-                    <a href="video.html" class="py-4 hover:text-stone-50">
-                        Video
-                    </a>
-                </li>
-                
-                
-                
             </ul>
 		`
     });
